a19a9fed835e8fd2aafcfb90d13f667c99034e57
arora <aroramadhav65@gmail.com>
2022-04-22 20:04:50 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),

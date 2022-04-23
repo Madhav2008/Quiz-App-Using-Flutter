a96619852b5e68c7bc259ff3cf60ab64a6cbc128
arora <aroramadhav65@gmail.com>
2022-04-23 23:26:04 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text('Quiz App'),
+      home: Center(child: const Text('Quiz App')),
     );
   }
 }

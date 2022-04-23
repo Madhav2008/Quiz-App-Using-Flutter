303a60cce224445f0b63359f7b5630ee62eb10ed
arora <aroramadhav65@gmail.com>
2022-04-23 23:28:49 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quiz App',
+      debugShowCheckedModeBanner: false, 
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

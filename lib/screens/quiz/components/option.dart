a432c9be75ddc0af7edbec29987711b6d5c9a762
arora <aroramadhav65@gmail.com>
2022-04-24 18:58:45 +0530

    Update

--- lib/screens/quiz/components/option.dart
+++ lib/screens/quiz/components/option.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:quiz_app/controllers/question_controller.dart';
-
 import '../../../constants.dart';
 
 class Option extends StatelessWidget {

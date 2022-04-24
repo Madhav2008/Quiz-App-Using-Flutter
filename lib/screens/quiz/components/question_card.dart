52ada6e70dcdfe80d257ef5169bde353f349a6b8
arora <aroramadhav65@gmail.com>
2022-04-24 19:00:40 +0530

    Update

--- lib/screens/quiz/components/question_card.dart
+++ lib/screens/quiz/components/question_card.dart
@@ -8,7 +8,6 @@ import 'option.dart';
 class QuestionCard extends StatelessWidget {
   const QuestionCard({
     Key? key,
-    // it means we have to pass this
     @required this.question,
   }) : super(key: key);
 

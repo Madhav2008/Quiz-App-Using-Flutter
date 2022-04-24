1ec13698cdcfa51d11f3d23ed6469c716ebfdd98
arora <aroramadhav65@gmail.com>
2022-04-24 19:01:55 +0530

    Update

--- lib/screens/quiz/quiz_screen.dart
+++ lib/screens/quiz/quiz_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:quiz_app/controllers/question_controller.dart';
-
 import 'components/body.dart';
 
 class QuizScreen extends StatelessWidget {
